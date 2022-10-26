4dd059e11525fe36438db9aa535761cb3a224c6f
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-26 00:17:27 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith('.txt'):
+            with open(path_file) as arquivo:
+                result = arquivo.read().split('\n')
+                return result
+        else:
+            return print('Formato inválido', file=sys.stderr)
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
