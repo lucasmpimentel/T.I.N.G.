582881c401a02277743841e9524b36a77585340a
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-25 23:33:57 -0300

    req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
+import queue
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
-
+        self._queue = list()
+    
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self._queue):
+            raise IndexError()
+        return self._queue[index]
