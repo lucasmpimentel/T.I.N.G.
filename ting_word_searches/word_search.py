bf773b630c48f983a7514ecba34a6e7be39f6564
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-11-01 03:11:43 -0300

    req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance._queue:
+        lista_palavras = []
+        for position, data in enumerate(item["linhas_do_arquivo"]):
+            if word.lower() in data.lower():
+                obj = {"linha": position + 1}
+                lista_palavras.append(obj)
+            if len(lista_palavras) == 0:
+                return []
+        response = {
+            "palavra": word,
+            "arquivo": lista_palavras["nome_do_arquivo"],
+            "ocorrencias": lista_palavras
+        }
+        result.append(response)
+    return result
 
 
 def search_by_word(word, instance):
