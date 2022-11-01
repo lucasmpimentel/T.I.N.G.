842ec6f8c668412a453eb6e05c07df90d1048efe
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-11-01 02:38:19 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    status = True
+    for item in instance._queue:
+        if item["nome_do_arquivo"] == path_file:
+            status = False
+            return status
+    if status:
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt_importer(path_file)),
+            "linhas_do_arquivo": (txt_importer(path_file))
+        }
+        instance.enqueue(result)
+        return print(result, file=sys.stdout)
 
 
 def remove(instance):
